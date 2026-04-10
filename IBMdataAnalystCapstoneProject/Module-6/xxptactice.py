c326cbdfed445710487e074a9be152fb07e16528
ledidk <ddkntakirutimana@gmail.com>
2026-04-10 16:07:56 -0400

    xx fie

---
+++ IBMdataAnalystCapstoneProject/Module-6/xxptactice.py
@@ -0,0 +1,16 @@
+thislist = ["apple", "apple", "cherry"]
+count = 0
+
+for a in thislist:
+	if a=="apple":
+		count += 1
+print(count)
+
+
+a= "dieudonne"
+b = ""
+for char in a:
+	if char =="d":
+		b += char
+        
+print(b)
